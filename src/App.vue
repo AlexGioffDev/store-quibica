dd720830b343d1afad88cab1d5899a3c298ec22e
Alex Gioffre <alex.gioffre_dev@outlook.it>
2026-07-18 19:32:52 +0200

    feat: Add modal for management the errors from API

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
 <script setup lang="ts">
+import BaseModal from './components/BaseModal.vue'
 import HeaderComponent from './components/HeaderComponent.vue'
+import { useErrorModal } from './stores/errorModal.ts'
+const errorModalStore = useErrorModal()
 </script>
 
 <template>
@@ -7,6 +10,13 @@ import HeaderComponent from './components/HeaderComponent.vue'
   <main class="container">
     <router-view />
   </main>
+  <BaseModal
+    :is-open="errorModalStore.isOpen"
+    title="Something went wrong"
+    @close="errorModalStore.closeError()"
+  >
+    <p>{{ errorModalStore.message }}</p>
+  </BaseModal>
 </template>
 
 <style scoped>
